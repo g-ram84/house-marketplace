c0b5297d0e7d4fcc97e93b2f83fddf2cb97015a8
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-12-17 12:57:16 -0800

    contact page created

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Listing from "./pages/Listing";
 import CreateListing from "./pages/CreateListing";
+import Contact from "./pages/Contact";
 
 function App() {
 	return (
@@ -32,6 +33,7 @@ function App() {
 					<Route path="/forgot-password" element={<ForgotPassword />} />
 					<Route path="/create-listing" element={<CreateListing />} />
 					<Route path="/offers" element={<Offers />} />
+					<Route path="/contact/:landlordId" element={<Contact />} />
 				</Routes>
 				<Navbar />
 			</Router>
